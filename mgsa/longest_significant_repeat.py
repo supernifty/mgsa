f3a01f466314ef1007ab6bcc40db6510211d7c2d
supernifty <peter@supernifty.org>
2015-08-02 21:54:54 +1000

    reverse complement on repeat searcher

--- mgsa/longest_significant_repeat.py
+++ mgsa/longest_significant_repeat.py
@@ -1,10 +1,12 @@
 # experimentally measures the longest repeated sequence expected by chance
 
+import argparse
 from itertools import imap, izip, starmap, tee
 from os.path   import commonprefix
 import random
 
 a = ('a', 'c', 'g', 't')
+r = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
 
 def pairwise(iterable): # itertools recipe
     a, b = tee(iterable)
@@ -26,7 +28,10 @@ def longest_duplicate_buffer(data):
     size, start = max(starmap(lcp_item, pairwise(sa)), key=lambda x: x[0])
     return data[start:start + size]
 
-def find_repeat( l, p ):
+def reverse_complement( s ):
+  return ''.join( [ r[c] for c in reversed(s) ] )
+
+def find_repeat( l, p, rc=False ):
   '''
     l is the string length
     p is the percentile
@@ -34,6 +39,8 @@ def find_repeat( l, p ):
   r = []
   for trial in xrange(100):
     s = ''.join( [ random.choice(a) for _ in xrange(l) ] )
+    if rc:
+      s = s + '*' + reverse_complement( s )
     r.append( len( longest_duplicate_buffer( s ) ) )
   r.sort()
   #print r
@@ -42,6 +49,9 @@ def find_repeat( l, p ):
 
     
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='random longest significant repeat')
+  parser.add_argument('--reverse_complement', dest='reverse_complement', type=bool, default=False, help='include reverse complement')
+  args = parser.parse_args() 
   for l in (16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384):
-    print l, find_repeat( l, 95 ) # prints length and longest repeat at 95th percentile
-  
+    print l, find_repeat( l, 95, rc=args.reverse_complement ) # prints length and longest repeat at 95th percentile
+
